ac57c12dbd5d4f0335abd24052a1d30c2f441ffb
Olenka-I <132247255+Olenka-I@users.noreply.github.com>
2023-05-12 00:28:33 +0200

    Updated time/date information

--- project.js
+++ project.js
@@ -1,3 +1,26 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return ` ${day} ${hours}:${minutes}`;
+}
+
 function displayWeatherData(data) {
   let cityName = data.name;
   let currentTemp = Math.round(data.main.temp);
@@ -5,12 +28,14 @@ function displayWeatherData(data) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
   document.querySelector(".topCity").textContent = cityName;
   document.querySelector(".temp").textContent = currentTemp;
 
   descriptionElement.innerHTML = data.weather[0].description;
   humidityElement.innerHTML = data.main.humidity;
   windElement.innerHTML = Math.round(data.wind.speed);
+  dateElement.innerHTML = formatDate(data.dt * 1000);
   iconElement.setAttribute(
     "src",
     `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
